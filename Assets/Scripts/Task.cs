c4fddf08eda69282bc292652acc8d77abc580f39
Sam Tuson <sam.tuson@hotmail.co.uk>
2020-11-29 15:29:34 +0000

    Reduce difficulty

--- Assets/Scripts/Task.cs
+++ Assets/Scripts/Task.cs
@@ -8,8 +8,8 @@ public class Task : MonoBehaviour {
     [SerializeField] private float baseMaxCooldown = 120f;
     private TaskStatus status = TaskStatus.repaired;
 
-    [SerializeField] private float criticalThreshold = -20f;
-    [SerializeField] private float gameOverThreshold = -30f;
+    [SerializeField] private float criticalThreshold = -30f;
+    [SerializeField] private float gameOverThreshold = -40f;
     [SerializeField] private float breakTimer;
 
     private float originalX;
