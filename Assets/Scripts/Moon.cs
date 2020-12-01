9d01bd4d9ac6899d39ca5f6a53c651e3e44b1d82
Sam Tuson <sam.tuson@hotmail.co.uk>
2020-12-01 12:11:09 +0000

    Slightly increase moon speed

--- Assets/Scripts/Moon.cs
+++ Assets/Scripts/Moon.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Moon : MonoBehaviour {
-    private float speed = 0.12f;
+    private float speed = 0.124f;
 
     void Update() {
         transform.position = new Vector3(transform.position.x, transform.position.y - speed * Time.deltaTime, transform.position.z);
